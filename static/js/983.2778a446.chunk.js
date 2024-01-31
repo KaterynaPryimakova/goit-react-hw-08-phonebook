9e9d055b2f5bb2496dabc65a14ed3fd27791810a
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-01-31 20:01:16 +0000

    Deploying to gh-pages from @ KaterynaPryimakova/goit-react-hw-08-phonebook@650c5bfb7324f15600c8a3f28c87565f4937a8db 🚀

---
+++ static/js/983.2778a446.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,r){r.r(t);r(2791);var c=r(3329);t.default=function(){return(0,c.jsx)("div",{children:(0,c.jsx)("h1",{children:"Welcome!"})})}}}]);
+//# sourceMappingURL=983.2778a446.chunk.js.map
