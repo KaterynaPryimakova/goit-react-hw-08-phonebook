f64a8e76f3bf00a6f8447604692f07cde4f667af
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-01-31 14:10:31 +0200

    new: registerUser, loginUser + logic in reducer

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,16 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { loginUser, registerUser } from './operations';
 
 const initialState = {
   token: null,
   error: null,
   userData: null,
   isLoading: false,
+  isLoggedIn: false,
 };
 
 const authSlice = createSlice({
   name: 'auth',
 
   initialState,
+
+  extraReducers: builder => {
+    builder
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isLoggedIn = true;
+        state.userData = action.payload.user;
+        state.token = action.payload.token;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isLoggedIn = true;
+        state.userData = action.payload.user;
+        state.token = action.payload.token;
+      })
+      .addMatcher(isAnyOf(registerUser.pending, loginUser.pending), state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addMatcher(
+        isAnyOf(registerUser.rejected, loginUser.rejected),
+        (state, action) => {
+          state.isLoading = false;
+          state.error = action.payload;
+        }
+      );
+  },
 });
 
 export const authReducer = authSlice.reducer;
---
+++ src/redux/auth/operations.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://connections-api.herokuapp.com',
+});
+
+const setToken = token => {
+  axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+// const removeToken = () => {
+//   axiosInstance.defaults.headers.common.Authorization = '';
+// };
+
+export const registerUser = createAsyncThunk(
+  'auth/registerUser',
+  async (userData, thunkAPI) => {
+    try {
+      const { data } = await axiosInstance.post('/users/signup', userData);
+      setToken(data.token);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  'auth/loginUser',
+  async (userData, thunkAPI) => {
+    try {
+      const { data } = await axiosInstance.post('/user/login/', userData);
+      setToken(data.token);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
